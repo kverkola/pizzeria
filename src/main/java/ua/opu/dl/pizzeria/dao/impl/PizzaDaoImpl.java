7833ce56a091b23371080393bd6f108b89495b17
muzNik <@nik>
2014-01-27 14:47:23 +0200

    add pizzadaoimpl loadByorder(without ingredients)

--- src/main/java/ua/opu/dl/pizzeria/dao/impl/PizzaDaoImpl.java
+++ src/main/java/ua/opu/dl/pizzeria/dao/impl/PizzaDaoImpl.java
@@ -5,83 +5,109 @@ import ua.opu.dl.pizzeria.model.Ingredient;
 import ua.opu.dl.pizzeria.model.Pizza;
 
 import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PizzaDaoImpl implements PizzaDao {
+	private JdbcTemplate jdbcTemplate;
+	private final String loadPizzaByOrderId = " with tab1 as(  select att.value name_,ob.OBJECT_ID id_,obj.OBJECT_TYPE_ID obtypeId from Attributes att, Objects ob, ATTRTYPE attr,OBJTYPE obj where  obj.OBJECT_TYPE_ID=ob.OBJECT_TYPE_ID and ob.object_id = att.object_id and att.attr_id=attr.attr_id and attr.code='NAME' ),tab2 as (  select att.value price, ob.OBJECT_ID id_,obj.OBJECT_TYPE_ID obtypeId from Attributes att, Objects ob, ATTRTYPE attr,OBJTYPE obj where obj.OBJECT_TYPE_ID=ob.OBJECT_TYPE_ID and ob.object_id = att.object_id and att.attr_id=attr.attr_id and attr.code='price' ),tab3 as (  select att.value logo, ob.OBJECT_ID id_,obj.OBJECT_TYPE_ID obtypeId from Attributes att, Objects ob, ATTRTYPE attr,OBJTYPE obj where  obj.OBJECT_TYPE_ID=ob.OBJECT_TYPE_ID and ob.object_id = att.object_id and att.attr_id=attr.attr_id and attr.code='logo'  ),tab4 as (  select att.value orderId, ob.OBJECT_ID id_,obj.OBJECT_TYPE_ID obtypeId from Attributes att, Objects ob, ATTRTYPE attr,OBJTYPE obj where obj.OBJECT_TYPE_ID=ob.OBJECT_TYPE_ID and ob.object_id = att.object_id and att.attr_id=attr.attr_id and attr.code='orderId' ),tab44 as (  select att.value description, ob.OBJECT_ID id_,obj.OBJECT_TYPE_ID obtypeId from Attributes att, Objects ob, ATTRTYPE attr,OBJTYPE obj where  obj.OBJECT_TYPE_ID=ob.OBJECT_TYPE_ID andob.object_id = att.object_id and att.attr_id=attr.attr_id and attr.code='description'  ),tab5 as( select tab1.id_ id,tab4.orderId orderId,tab1.name_ name,tab2.price price,tab3.logo logo,tab44.description description from tab1,tab2,tab3,tab4,tab44 where tab1.ID_=tab2.ID_ and tab2.ID_=tab3.ID_ and tab3.Id_=tab4.Id_ and tab4.Id_=tab44.Id_ and tab1.obtypeId=tab2.obtypeId and tab2.obtypeId=tab3.obtypeId and tab3.obtypeId=tab4.obtypeId and tab4.obtypeId=tab44.obtypeId and tab1.obtypeId=5 )select* from tab5 where orderId=?";
 
-    private DataSource dataSource;
+	// private NamedParameterJdbcTemplate namedTemplate;
 
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		// this.namedTemplate = new NamedParameterJdbcTemplate(dataSource);
+	}
 
-    @Override
-    public void addPizza(Pizza pizza) {
+	private RowMapper<Pizza> rowMapper = new RowMapper<Pizza>() {
+		@Override
+		public Pizza mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Pizza pizza = new Pizza();
+			pizza.setId(rs.getInt("id"));
+			pizza.setOrderId(rs.getInt("orderId"));
+			pizza.setName(rs.getString("name"));
+			pizza.setPrice(rs.getDouble("price"));
+			pizza.setLogo(rs.getString("logo"));
+			pizza.setLogo(rs.getString("description"));
 
-    }
+			return pizza;
+		}
+	};
 
-    @Override
-    public void updatePizza(Pizza pizza) {
+	@Override
+	public void addPizza(Pizza pizza) {
 
-    }
+	}
 
-    @Override
-    public void deletePizza(Pizza pizza) {
+	@Override
+	public void updatePizza(Pizza pizza) {
 
-    }
+	}
 
-    /**
-     * Currently this method don't return real object from db
-     * @param id
-     * @return
-     */
-    @Override
-    public Pizza loadById(Integer id) {
+	@Override
+	public void deletePizza(Pizza pizza) {
 
-        Map<Ingredient, Integer> ingredientMap = new HashMap();
-        ingredientMap.put(new Ingredient("Ham", 100, 30), 1);
-        ingredientMap.put(new Ingredient("Vegetables", 80, 10), 1);
-        ingredientMap.put(new Ingredient("Cheese", 90, 20), 1);
-        ingredientMap.put(new Ingredient("Sauce", 60, 15), 1);
-        ingredientMap.put(new Ingredient("Crust", 300, 30), 1);
+	}
 
-        return new Pizza("TestPizza " + id, ingredientMap,
-                "chikenita_middle.png", "wwwwwwwwwwwewert", id, 45);
-    }
+	/**
+	 * Currently this method don't return real object from db
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@Override
+	public Pizza loadById(Integer id) {
 
-    @Override
-    public List<Pizza> loadByOrder(Integer orderId) {
+		Map<Ingredient, Integer> ingredientMap = new HashMap();
+		ingredientMap.put(new Ingredient("Ham", 100, 30), 1);
+		ingredientMap.put(new Ingredient("Vegetables", 80, 10), 1);
+		ingredientMap.put(new Ingredient("Cheese", 90, 20), 1);
+		ingredientMap.put(new Ingredient("Sauce", 60, 15), 1);
+		ingredientMap.put(new Ingredient("Crust", 300, 30), 1);
 
-        List<Pizza> pizzaList = new ArrayList();
+		return new Pizza("TestPizza " + id, ingredientMap,
+				"chikenita_middle.png", "wwwwwwwwwwwewert", id, 45);
+	}
 
-        return pizzaList;
-    }
+	@Override
+	public List<Pizza> loadByOrder(Integer orderId) {
 
-    @Override
-    public List<Pizza> loadAll() {
+		return jdbcTemplate.query(loadPizzaByOrderId, rowMapper,
+				String.valueOf(orderId));
+	}
 
-        List<Pizza> pizzaList = new ArrayList();
+	@Override
+	public List<Pizza> loadAll() {
 
-        Map<Ingredient, Integer> ingredientMap = new HashMap();
-        ingredientMap.put(new Ingredient("Ham", 100, 30), 1);
-        ingredientMap.put(new Ingredient("Vegetables", 80, 10), 1);
-        ingredientMap.put(new Ingredient("Cheese", 90, 20), 1);
-        ingredientMap.put(new Ingredient("Sauce", 60, 15), 1);
-        ingredientMap.put(new Ingredient("Crust", 300, 30), 1);
+		List<Pizza> pizzaList = new ArrayList();
 
-        String[] imgs = {"chikenita_middle.png", "img_2.png", "pizza_middle.png", "tanu_mini.png"};
-        String[] descrips = {"Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-                "Aenean commodo ligula eget dolor. Aenean massa.",
-                "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
-                "Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim."
-        };
+		Map<Ingredient, Integer> ingredientMap = new HashMap();
+		ingredientMap.put(new Ingredient("Ham", 100, 30), 1);
+		ingredientMap.put(new Ingredient("Vegetables", 80, 10), 1);
+		ingredientMap.put(new Ingredient("Cheese", 90, 20), 1);
+		ingredientMap.put(new Ingredient("Sauce", 60, 15), 1);
+		ingredientMap.put(new Ingredient("Crust", 300, 30), 1);
 
-        for (int i = 0; i < imgs.length; i++) {
-            pizzaList.add(new Pizza("TestPizza " + i, ingredientMap, imgs[i], descrips[i], i, 45));
-        }
-        return pizzaList;
-    }
+		String[] imgs = { "chikenita_middle.png", "img_2.png",
+				"pizza_middle.png", "tanu_mini.png" };
+		String[] descrips = {
+				"Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+				"Aenean commodo ligula eget dolor. Aenean massa.",
+				"Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+				"Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim." };
+
+		for (int i = 0; i < imgs.length; i++) {
+			pizzaList.add(new Pizza("TestPizza " + i, ingredientMap, imgs[i],
+					descrips[i], i, 45));
+		}
+		return pizzaList;
+	}
 }
--- src/main/java/ua/opu/dl/pizzeria/model/Pizza.java
+++ src/main/java/ua/opu/dl/pizzeria/model/Pizza.java
@@ -4,80 +4,99 @@ import java.util.Map;
 
 public class Pizza extends Product {
 
-    private Map<Ingredient, Integer> map;
-    private String logo;
-    private String description;
+	private Map<Ingredient, Integer> map;
+	private int orderId;
+	private String logo;
+	private String description;
+	public Pizza() {
+	}
 
-    public Pizza(String name, Map<Ingredient, Integer> map, String logo,
-                 String description, Integer id, double price) {// не забыть удалить
-        // id из
-        // конструктора
-        super(id, name, 1, price);
-        this.map = map;
-        this.logo = logo;
-        this.description = description;
-        countTotalPrice();
-    }
+	public Pizza(String name, Map<Ingredient, Integer> map, String logo,
+			String description, Integer id, double price) {// не забыть удалить
+		// id из
+		// конструктора
+		super(id, name, 1, price);
+		this.map = map;
+		this.logo = logo;
+		this.description = description;
+		countTotalPrice();
+	}
 
-    public Map<Ingredient, Integer> getMap() {
-        return map;
-    }
+	public Map<Ingredient, Integer> getMap() {
+		return map;
+	}
 
-    public void setMap(Map<Ingredient, Integer> map) {
-        this.map = map;
-    }
+	public void setMap(Map<Ingredient, Integer> map) {
+		this.map = map;
+	}
 
-    public String getLogo() {
-        return logo;
-    }
+	public String getLogo() {
+		return logo;
+	}
 
-    public void setLogo(String logo) {
-        this.logo = logo;
-    }
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public void countTotalPrice() {
-        double price = 0;
-        Integer key;
-        Map<Ingredient, Integer> ingrMap = getMap();
-        for (Ingredient ingr : ingrMap.keySet()) {
-            key = ingrMap.get(ingr);
-            price = price + ingr.getPrice() * key;
-        }
-        setPrice(price);
-    }
+	public int getOrderId() {
+		return orderId;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
 
-        Pizza pizza = (Pizza) o;
+	public void countTotalPrice() {
+		double price = 0;
+		Integer key;
+		Map<Ingredient, Integer> ingrMap = getMap();
+		for (Ingredient ingr : ingrMap.keySet()) {
+			key = ingrMap.get(ingr);
+			price = price + ingr.getPrice() * key;
+		}
+		setPrice(price);
+	}
 
-        if (Double.compare(pizza.getPrice(), getPrice()) != 0) return false;
-        if (getId() != null ? !getId().equals(pizza.getId()) : pizza.getId() != null) return false;
-        if (map != null ? !map.equals(pizza.map) : pizza.map != null) return false;
-        if (getName() != null ? !getName().equals(pizza.getName()) : pizza.getName() != null) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-        return true;
-    }
+		Pizza pizza = (Pizza) o;
 
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        result = getId() != null ? getId().hashCode() : 0;
-        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
-        result = 31 * result + (map != null ? map.hashCode() : 0);
-        temp = Double.doubleToLongBits(getPrice());
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
+		if (Double.compare(pizza.getPrice(), getPrice()) != 0)
+			return false;
+		if (getId() != null ? !getId().equals(pizza.getId())
+				: pizza.getId() != null)
+			return false;
+		if (map != null ? !map.equals(pizza.map) : pizza.map != null)
+			return false;
+		if (getName() != null ? !getName().equals(pizza.getName()) : pizza
+				.getName() != null)
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result;
+		long temp;
+		result = getId() != null ? getId().hashCode() : 0;
+		result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+		result = 31 * result + (map != null ? map.hashCode() : 0);
+		temp = Double.doubleToLongBits(getPrice());
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
 }
